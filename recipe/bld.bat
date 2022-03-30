f9e5a32bef16fb02cdcd93ecb3a84240aee61004
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-03-29 22:01:20 -0500

    add windows

---
+++ recipe/bld.bat
@@ -0,0 +1,24 @@
+@echo on
+
+:: delete CI variable for build
+set CI_BAK=%CI%
+
+set CI=
+
+:: build gradle
+./gradlew installAll -Pgradle_installPath=./tmp/BUILD_GRADLE
+
+:: restore variable
+set CI=%CI_BAK%
+
+:: create output folder name
+VERSION="%PKG_NAME%-%PKG_VERSION%"
+OUT="%PREFIX%\share\%VERSION%"
+
+:: copy the files to /share/%VERSION%
+md "%OUT%"
+copy .\tmp\BUILD_GRADLE\* "%OUT%\."
+
+:: create wrapper
+echo %OUT%\bin\gradle.exe %%* > %LIBRARY_BIN%\gradle.cmd
+echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\gradle.cmd
