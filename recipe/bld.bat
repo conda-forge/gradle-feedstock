aef4980247a54166b8313d88423cb5b8b9dc489c
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-03-30 20:04:04 -0500

    further simplify

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,24 +3,18 @@
 :: delete CI variable for build
 set CI=
 
-dir
-
-set GRADLE_ARGS=install -Pgradle_installPath=.\tmp\BUILD_GRADLE -x docs --stacktrace
-
-:: build gradle, retrying a couple times to account for network flake
-.\gradlew %GRADLE_ARGS% ^
-    || .\gradlew %GRADLE_ARGS% ^
-    || .\gradlew %GRADLE_ARGS%
-
-dir .\tmp\BUILD_GRADLE
-
 :: create output folder name
 VERSION="%PKG_NAME%-%PKG_VERSION%"
 OUT="%PREFIX%\share\%VERSION%"
-
-:: copy the files to /share/%VERSION%
 md "%OUT%"
-copy .\tmp\BUILD_GRADLE\* "%OUT%\."
+
+:: build gradle, retrying a couple times to account for network flake
+.\gradlew install -Pgradle_installPath="%OUT%" -x docs --stacktrace
+.\gradlew install -Pgradle_installPath="%OUT%" -x docs --stacktrace
+.\gradlew install -Pgradle_installPath="%OUT%" -x docs --stacktrace
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+dir %OUT% || exit 1
 
 :: create wrapper
 echo %OUT%\bin\gradle.exe %%* > %LIBRARY_BIN%\gradle.cmd
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,20 +5,16 @@ set -eux -o pipefail
 # delete CI variable for build
 unset CI
 
-GRADLE_ARGS="install -Pgradle_installPath=./tmp/BUILD_GRADLE -x docs --stacktrace"
-
-# build gradle, retrying a couple times to account for network flake
-./gradlew $GRADLE_ARGS \
-    || ./gradlew $GRADLE_ARGS \
-    || ./gradlew $GRADLE_ARGS
-
 # create output folder name
 VERSION="${PKG_NAME}-${PKG_VERSION}"
 OUT="${PREFIX}/share/${VERSION}"
-
-# copy the files to /share/${VERSION}
 mkdir -p "${OUT}"
-cp -R ./tmp/BUILD_GRADLE/* "${OUT}/."
+
+# build gradle, retrying a couple times to account for network flake
+GRADLE_ARGS="install -Pgradle_installPath=${OUT} -x docs --stacktrace"
+./gradlew $GRADLE_ARGS \
+    || ./gradlew $GRADLE_ARGS \
+    || ./gradlew $GRADLE_ARGS
 
 # create symlink
 ln -s "${OUT}/bin/gradle" "${PREFIX}/bin/gradle"
