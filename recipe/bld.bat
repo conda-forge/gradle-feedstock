97eb570bac7082f8cbc7afd97ec1986e4ac72ac6
Adrian Freund <adrian@freund.io>
2024-08-13 22:58:28 +0200

    Make package noarch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,12 +10,8 @@ set OUT=%PREFIX%\share\%VERSION%
 :: build gradle
 call .\gradlew install -Pgradle_installPath=%OUT% -x docs --stacktrace
 
-dir %OUT%
-dir %OUT%\bin
-dir %OUT%\lib
-
 :: create wrapper
-echo call %OUT%\bin\gradle.bat %%* > %LIBRARY_BIN%\gradle.cmd
-echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\gradle.cmd
+echo call %OUT%\bin\gradle.bat %%* > %LIBRARY_BIN%\gradle.bat
+echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\gradle.bat
 
-type %LIBRARY_BIN%\gradle.cmd
+type %LIBRARY_BIN%\gradle.bat
